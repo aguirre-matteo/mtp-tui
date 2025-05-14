8b8ca025156751ff5e14240a2435845422eb43fd
Aguirre Matteo <aguirre.matteo77@gmail.com>
2025-05-14 16:41:39 -0300

    Add custom config flag

--- config/config.go
+++ config/config.go
@@ -2,17 +2,41 @@ package config
 
 import (
 	"github.com/aguirre-matteo/mtp-tui/checks"
+	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
-func InitViper(user string) error {
+func InitViper(cmd *cobra.Command) error {
+	user, err := cmd.Flags().GetString("user")
+	if err != nil {
+		return err
+	}
+
 	cfgPath, err := getCfgPath(user)
+	if err != nil {
+		return err
+	}
+
+	overrideCfgPath, err := cmd.Flags().GetString("config")
 	if err != nil {
 		return err
 	}
 
 	viper.SetConfigName("mtp-tui")
 	viper.SetConfigType("yaml")
+
+	if overrideCfgPath != "unset" {
+		err = assertConfigPath(overrideCfgPath)
+		if err != nil {
+			return err
+		}
+		viper.AddConfigPath(overrideCfgPath)
+	}
+
+	err = assertConfigPath(cfgPath)
+	if err != nil {
+		return err
+	}
 	viper.AddConfigPath(cfgPath)
 	viper.AddConfigPath("/etc/")
 
